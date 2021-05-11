95861ee55228aa42411bb9b835a7e790d64bcc29
Salah-Eddine Nourelayne <senourelayne@cegid.com>
2021-05-11 14:46:06 +0200

    Routing Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
-import { convertActionBinding } from '@angular/compiler/src/compiler_util/expression_converter';
-import { convertMetaToOutput } from '@angular/compiler/src/render3/util';
 import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpaces,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+       {path: 'products', component: ProductListComponent},
+       {path: 'products/:id', component: ProductDetailComponent},
+       {path: 'welcome', component: WelcomeComponent},
+       {path: '', redirectTo: 'welcome' , pathMatch:  'full'},
+       {path: '**', redirectTo: 'welcome' , pathMatch:  'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from './product.service';
 
 @Component({
-   selector: "pm-products",
    templateUrl: "./product-list.component.html",
    styleUrls : ["./product-list.component.css"]
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
    pageTitle: string = 'Product List';
    showImage : boolean  = false;
    private _listFilter: string = "";
    filtredProducts : IProduct[] = [];
    products: IProduct[] = [];
    private  _productService;
-
+  sub ! : Subscription;
+  errorMessage: string = '';
    constructor(productService : ProductService){
        this._productService = productService;
    }
@@ -45,7 +46,16 @@ export class ProductListComponent implements OnInit{
    }
 
    ngOnInit(): void {
-       this.products = this._productService.getProducts();
-       this.filtredProducts = this.products;
+       this.sub = this._productService.getProducts().subscribe({
+           next : products => {
+               this.products = products;
+               this.filtredProducts = this.products;
+           },
+           error: err  =>  this.errorMessage = err
+       });
+  }
+
+  ngOnDestroy(){
+      this.sub.unsubscribe();
   }
 }
